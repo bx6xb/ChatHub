d0e594d0e69d2b03e96fba4be469c55a0f7d1a62
Yan Yu <bx6xbChosen1@gmail.com>
2024-05-14 22:29:12 +0600

    rewrote forms with redux-form

--- src/pages/Dialogs/Dialogs.tsx
+++ src/pages/Dialogs/Dialogs.tsx
@@ -5,6 +5,7 @@ import { Message } from "./Message/Message"
 import { addMessageAC, updateNewMessageAC } from "../../redux/dialogsReducer/dialogsReducer"
 import { useAppDispatch, useAppSelector } from "../../redux/store"
 import { withAuthRedirect } from "../../hoc/withAuthRedirect"
+import { Field, InjectedFormProps, reduxForm } from "redux-form"
 
 export const Dialogs = withAuthRedirect(() => {
   const { dialogs, messages, newMessageText } = useAppSelector((state) => state.dialogs)
@@ -16,6 +17,11 @@ export const Dialogs = withAuthRedirect(() => {
   const updateNewMessage = (e: ChangeEvent<HTMLTextAreaElement>) => {
     dispatch(updateNewMessageAC(e.currentTarget.value))
   }
+  const onSubmit = (formData: DialogsFormData) => {
+    console.log(formData)
+    dispatch(updateNewMessageAC(formData.message))
+    dispatch(addMessageAC())
+  }
 
   let dialogsElements = dialogs.map((u) => <DialogItem key={u.id} id={u.id} name={u.name} />)
   let messagesElements = messages.map((m) => <Message key={m.id} message={m.message} />)
@@ -24,8 +30,24 @@ export const Dialogs = withAuthRedirect(() => {
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>{dialogsElements}</div>
       <div className={s.messages}>{messagesElements}</div>
-      <textarea value={newMessageText} onChange={updateNewMessage} />
-      <button onClick={sendMessage}>Submit</button>
+      {/* <textarea value={newMessageText} onChange={updateNewMessage} />
+      <button onClick={sendMessage}>Submit</button> */}
+      <DialogsReduxForm onSubmit={onSubmit} />
     </div>
   )
 })
+
+type DialogsFormData = {
+  message: string
+}
+
+const DialogsForm: React.FC<InjectedFormProps<DialogsFormData>> = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <Field component={"textarea"} name="message" placeholder="message" />
+      <button>Submit</button>
+    </form>
+  )
+}
+
+const DialogsReduxForm = reduxForm<DialogsFormData>({ form: "dialogs" })(DialogsForm)
--- src/pages/Profile/MyPosts/MyPosts.tsx
+++ src/pages/Profile/MyPosts/MyPosts.tsx
@@ -1,8 +1,9 @@
-import { ChangeEvent } from "react"
+import React, { ChangeEvent } from "react"
 import s from "./MyPosts.module.css"
 import { Post } from "./Post/Post"
 import { addPostAC, updateNewPostAC } from "../../../redux/profileReducer/profileReducer"
 import { useAppDispatch, useAppSelector } from "../../../redux/store"
+import { Field, InjectedFormProps, reduxForm } from "redux-form"
 
 export const MyPosts = () => {
   const { newPostText, posts } = useAppSelector((state) => state.profile)
@@ -11,30 +12,52 @@ export const MyPosts = () => {
   const addPost = () => {
     dispatch(addPostAC())
   }
-
-  let postsElements = posts.map((p) => (
-    <Post key={p.id} message={p.message} likesCount={p.likesCount} />
-  ))
-
   const onPostChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     let text = e.currentTarget.value
     if (text.trim()) {
       dispatch(updateNewPostAC(text))
     }
   }
+  const onSubmit = (formData: PostsFormData) => {
+    dispatch(updateNewPostAC(formData.message))
+    dispatch(addPostAC())
+  }
+
+  let postsElements = posts.map((p) => (
+    <Post key={p.id} message={p.message} likesCount={p.likesCount} />
+  ))
 
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
-      <div>
+      {/* <div>
         <div>
           <textarea value={newPostText} onChange={onPostChange} />
         </div>
         <div>
           <button onClick={addPost}>Add post</button>
         </div>
-      </div>
+      </div> */}
+      <PostsReduxForm onSubmit={onSubmit} />
       <div className={s.posts}>{postsElements}</div>
     </div>
   )
 }
+
+type PostsFormData = {
+  message: string
+}
+
+const PostsForm: React.FC<InjectedFormProps<PostsFormData>> = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field name="message" component={"textarea"} />
+      </div>
+      <div>
+        <button>Add post</button>
+      </div>
+    </form>
+  )
+}
+const PostsReduxForm = reduxForm<PostsFormData>({ form: "posts" })(PostsForm)
