7b43f545f5c7955f9268dc949753eb1fe7f26a84
Yan Turnt <bx6xbChosen1@gmail.com>
2024-05-30 15:56:08 +0600

    added gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import "./index.css"
 import reportWebVitals from "./reportWebVitals"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import App from "./App"
 import ReactDOM from "react-dom/client"
 import { store } from "./redux/store"
@@ -9,11 +9,11 @@ import { Provider } from "react-redux"
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 )
 
 // If you want to start measuring performance in your app, pass a function
